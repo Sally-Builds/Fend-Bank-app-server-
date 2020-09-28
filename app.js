2e48d05bd084ce72cf1d86308730443b22cca4a3
Uzoagulu Joshua <uzoagulujoshua@yahoo.com>
2020-09-29 00:32:43 +0100

    imgaes change

--- app.js
+++ app.js
@@ -39,7 +39,8 @@ app.use((req, res, next) => {
   next();
 });
 //serving static files
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(`${path.join(__dirname)}/public/img`));
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/accounts', accountRouter);
