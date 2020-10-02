47b5710ed1e2b1aa56f80c2c439ece748ca17177
Uzoagulu Joshua <uzoagulujoshua@yahoo.com>
2020-10-03 00:21:27 +0100

    added static folders

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const compression = require('compression');
 const userRouter = require('./routes/userRoutes');
 const accountRouter = require('./routes/accountRoutes');
 const transactionRouter = require('./routes/transactionRoutes');
-const AppError = require('./utils/appError');
+// const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
 
 const app = express();
@@ -30,22 +30,6 @@ app.options('*', cors());
 app.use(express.json());
 app.use(cookieParser());
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Orign, X-Requested-With, Content-Type, Accept, Authorization'
-//   );
-//   if (req.method === 'OPTIONS') {
-//     res.header(
-//       'Access-Control-Allow-Methods',
-//       'PUT POST PATCH DELETE POST GET'
-//     );
-//     return res.status.json({});
-//   }
-//   next();
-// });
-
 //compress responses and request
 app.use(compression());
 
@@ -56,15 +40,20 @@ app.use((req, res, next) => {
 });
 //serving static files
 // app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images', express.static(`${path.join(__dirname)}/public/img`));
+// app.use('/images', express.static(`${path.join(__dirname)}/public/img`));
 
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/accounts', accountRouter);
 app.use('/api/v1/transactions', transactionRouter);
 
-app.all('*', (req, res, next) => {
-  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(`${__dirname}public`));
+  app.get(/.*/, (req, res) => res.sendFile(`${__dirname}public/index`));
+}
+
+// app.all('*', (req, res, next) => {
+//   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+// });
 
 app.use(globalErrorHandler);
 
